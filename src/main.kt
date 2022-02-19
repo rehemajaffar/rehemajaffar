5a6a7298a422989dc7c15ce89f3cdad4e7c69e92
rehemajaffar <rehemajumajaffar@gmail.com>
2022-02-19 09:55:29 +0300

    variables and data types correction

---
+++ src/main.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var name ="students"
+    var age ="age numbers"
+    var weight ="weight number"
+    var kenya = true
+    var notKenyan = false
+
+    println(name)
+    println(age)
+    println(weight)
+    println(kenya) //outputs true
+    println(notKenyan) //outputs false
+}
